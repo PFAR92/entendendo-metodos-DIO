545ff11b7090450a3127a02368d324d1f45a0278
PFAR92 <paulofelipetj@hotmail.com>
2022-09-19 10:39:33 -0300

    19/19/22 10:39

--- quadrilaterosNotaveis/src/App.java
+++ quadrilaterosNotaveis/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
+        System.out.println("Exercício Quadrilátero retornando valores (sobrecarga de um método)");
+
+        System.out.println(Quadrilatero.area(3));
+        System.out.println(Quadrilatero.area(5, 5));
+        System.out.println(Quadrilatero.area(7, 8, 9));
+      
+
+
     }
 }
