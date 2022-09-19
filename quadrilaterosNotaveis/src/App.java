53c261d4cf440ee76ed4c1187e61903ddc87ab13
PFAR92 <paulofelipetj@hotmail.com>
2022-09-19 10:43:53 -0300

    19/09/22 10:43

--- quadrilaterosNotaveis/src/App.java
+++ quadrilaterosNotaveis/src/App.java
@@ -3,9 +3,9 @@ public class App {
         
         System.out.println("Exercício Quadrilátero retornando valores (sobrecarga de um método)");
 
-        System.out.println(Quadrilatero.area(3));
-        System.out.println(Quadrilatero.area(5, 5));
-        System.out.println(Quadrilatero.area(7, 8, 9));
+        System.out.println("Área do quadrado" + Quadrilatero.area(3));
+        System.out.println("Área do retãngulo" + Quadrilatero.area(5, 5));
+        System.out.println("Área do trapézio" + Quadrilatero.area(7, 8, 9));
       
 
 
